17d5db2fad2df040623c0c870a5a35f49f634429
savkirill2012 <savkirill2012@gmail.com>
2023-04-23 22:24:07 +0300

    add flags serch logic

--- src/main/s21_cat/s21_cat.c
+++ src/main/s21_cat/s21_cat.c
@@ -26,12 +26,16 @@ int get_flags_from_consol_input(Flags_cat *flags, const int num_elem, const char
     for (int i = 1; i < num_elem; i++) {
         if (consol_input[i][0] == '-') {
             if (consol_input[i][1] == '-') {
-                //check for flag
+                error_flag = add_gnu_flags_to_struct(flags, consol_input[i]);
             } else {
                 error_flag = add_flags_to_file_struct(flags, consol_input[i]);
             }
         } else {
             //add file
+        }
+
+        if (error_flag == -1) {
+            break;
         } 
     }
 
@@ -44,7 +48,9 @@ int add_flags_to_file_struct(Flags_cat *flags, const char *row) {
 
     for (int i = 1; i < len; i++) {
         switch (row[i]) {
-            case 'e': flags->flag_e = 1; 
+            case 'e': 
+                flags->flag_e = 1;
+                flags->flag_v = 1; 
                 break;
             case 'E': flags->flag_e = 1; 
                 break;
@@ -65,6 +71,33 @@ int add_flags_to_file_struct(Flags_cat *flags, const char *row) {
             printf("Error can't search the flag %c\n", row[i]);
             break;
         }
+    }
+
+    return error_flag;
+}
+
+int add_gnu_flags_to_struct(Flags_cat *flags, const char *row) {
+    int error_flag = -1;
+    char *gnu_flags[] = {"--number-nonblank", "--number", "--squeeze-blank"};
+
+    for (int i = 0; i < 3; i++) {
+        if (strcmp(row, gnu_flags[i]) == 0) {
+            switch (i){
+                case 0: flags->flag_b = 1;
+                    break;
+                case 1: flags->flag_n = 1;
+                    break;
+                case 2: flags->flag_s = 1;
+                    break;
+            }
+            
+            error_flag = 0;
+            break;
+        }
+    }
+
+    if (error_flag == -1) {
+        printf("Can't find this flag: %s\n", row);
     }
 
     return error_flag;
